fc87ace9cd2d89f26221dd047345ee37ce0c150e
Terry27 <155521738+ASEMAHLE27@users.noreply.github.com>
2025-05-29 11:33:14 +0200

    Improved Home page for admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,43 +5,43 @@
           
             <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mb-8 ">
                 <div class="bg-white rounded-md shadow-md p-4 flex flex-col justify-between">
-                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Total Vacancies/Job</h2>
-                    <div class="text-3xl font-bold text-purple-500">#</div>
-                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Vacancies/Job</a>
+                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Total Leaves</h2>
+                    <div class="text-2xl font-bold text-gray-800">#</div>
+                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Total Leaves</a>
                 </div>
                 <div class="bg-white rounded-md shadow-md p-4 flex flex-col justify-between">
-                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Total Job Applications</h2>
-                    <div class="text-3xl font-bold text-purple-500">#</div>
-                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Vacancies/Job Applications</a>
+                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Pending Leaves</h2>
+                    <div class="text-2xl font-bold text-gray-800">#</div>
+                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">ViewPending Leaves</a>
                 </div>
                 <div class="bg-white rounded-md shadow-md p-4 flex flex-col justify-between">
-                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Active Vacancies/Job  </h2>
-                    <div class="text-3xl font-bold text-purple-500">#</div>
-                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Active Vacancies/Job</a>
+                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Approved Leaves  </h2>
+                    <div class="text-2xl font-bold text-gray-800">#</div>
+                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Approved Leaves</a>
                 </div>
                 <div class="bg-white rounded-md shadow-md p-4 flex flex-col justify-between">
-                    <h2 class="text-lg font-semibold text-gray-700 mb-2">New Job Applications</h2>
-                    <div class="text-3xl font-bold text-purple-500">#</div>
-                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View New Job Applications</a>
+                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Total Users</h2>
+                    <div class="text-2xl font-bold text-gray-800">#</div>
+                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Users</a>
                 </div>
                 <div class="bg-white rounded-md shadow-md p-4 flex flex-col justify-between">
-                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Selected Applicants</h2>
-                    <div class="text-3xl font-bold text-purple-500">#</div>
-                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Selected Applicants</a>
+                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Holidays</h2>
+                    <div class="text-2xl font-bold text-gray-800">#</div>
+                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Holidays</a>
                 </div>
                 <div class="bg-white rounded-md shadow-md p-4 flex flex-col justify-between">
-                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Rejected Job Applications</h2>
-                    <div class="text-3xl font-bold text-purple-500">#</div>
-                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Rejected Job Applications</a>
+                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Leave Types</h2>
+                    <div class="text-2xl font-bold text-gray-800">#</div>
+                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Leave Types</a>
                 </div>
                 <div class="bg-white rounded-md shadow-md p-4 flex flex-col justify-between">
-                    <h2 class="text-lg font-semibold text-gray-700 mb-2">Total Job </h2>
-                    <div class="text-3xl font-bold text-purple-500">0</div>
-                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Total Job </a>
+                    <h2 class="text-lg font-semibold text-gray-700 mb-2">On Leave </h2>
+                    <div class="text-2xl font-bold text-gray-800">0</div>
+                    <a href="#" class="text-blue-500 hover:underline text-sm mt-4">On Leave </a>
                 </div>
                 <div class="bg-white rounded-md shadow-md p-4 flex flex-col justify-between">
                     <h2 class="text-lg font-semibold text-gray-700 mb-2">Messages</h2>
-                    <div class="text-3xl font-bold text-purple-500">0</div>
+                    <div class="text-2xl font-bold text-gray-800">0</div>
                     <a href="#" class="text-blue-500 hover:underline text-sm mt-4">View Messages</a>
                 </div>
                 
